ae51c14c4c605b6ed7c4dfe129005be02701ab91
vyten <vyten@munet.nu>
2017-06-09 14:07:17 +0200

    First

---
+++ starnet/tinymvc/myapp/controllers/default.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * default.php
+ *
+ * default application controller
+ *
+ * @package		TinyMVC
+ * @author		Monte Ohrt
+ */
+
+class Default_Controller extends TinyMVC_Controller
+{
+  function index() {
+    $this->view->display('index_view');
+  }
+  
+  function info() {
+  	$this->view->display('info_view');
+  }
+}
+
+?>
